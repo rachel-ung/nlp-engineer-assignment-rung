5050e9929f2198f38953b2b4cbb05a1b58098134
rachel-ung <108318316+rachel-ung@users.noreply.github.com>
2024-01-19 16:18:29 +0900

    Update api.py

--- src/nlp_engineer_assignment/api.py
+++ src/nlp_engineer_assignment/api.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from fastapi.logger import logger
 from starlette.responses import RedirectResponse
+from pydantic import BaseModel
+from .transformer import predict_freq
 
 
+x
 app = FastAPI(
     title="NLP Engineer Assignment",
     version="1.0.0"
@@ -19,3 +23,27 @@ async def index():
 # TODO: Add a route to the API that accepts a text input and uses the trained
 # model to predict the number of occurrences of each letter in the text up to
 # that point.
+
+class InputText(BaseModel):
+    text: str
+
+class OutputText(BaseModel):
+    prediction: str
+
+@app.post("/count/", response_model=OutputText)
+def count_freq(body: InputText):
+    logger.info(f'text: {body}')
+    pred = predict_freq("trained_model", body.text)
+    output = {
+        "prediction": pred
+    }
+    logger.info(f'prediction: {pred}')
+    return output
+
+@app.get("/", include_in_schema=False)
+async def index():
+    """
+    Redirects to the OpenAPI Swagger UI
+    """
+    return RedirectResponse(url="/docs")
+
